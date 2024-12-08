4e4e0877690b99a44e920483cfa2f07bc8733d75
paraspalawat <paraspalawat187@gmail.com>
2024-12-08 19:52:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+// script.js
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+function toggleNav() {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('active');
+}
